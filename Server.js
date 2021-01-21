b08020246903fafff4c512f77716ad20641e9167
Rodrigo Cortez Villalobos <rodrigo_0.3@outlook.com>
2021-01-21 15:11:44 -0300

    Primer Comit

---
+++ Server.js
@@ -0,0 +1,37 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+///////PAGINA DEL HOME
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: "Rodrigo",
+
+    });
+});
+////// Pagina renderizando el about
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+
+
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
